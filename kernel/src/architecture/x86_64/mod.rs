63e750c9162410058f32fecdfadfe770cf93ba25
Foldr <rightfold+kloe@gmail.com>
2018-11-22 01:10:25 +0100

    Add first_page_table

--- kernel/src/architecture/x86_64/mod.rs
+++ kernel/src/architecture/x86_64/mod.rs
@@ -1,3 +1,13 @@
 //! x86-64-specific code.
 
 pub mod paging;
+
+/// Processor state.
+pub struct Processor;
+
+impl Processor {
+    /// The value inside the CR3 register.
+    pub fn cr3(&self) -> u64 {
+        unimplemented!();
+    }
+}
--- kernel/src/architecture/x86_64/paging.rs
+++ kernel/src/architecture/x86_64/paging.rs
@@ -1,6 +1,7 @@
 //! x86-64 paging, using page map level four, also known as PML4.
 
 use address::PhysicalAddress;
+use architecture::x86_64::Processor;
 
 /// Page table.
 #[repr(C, packed)]
@@ -11,6 +12,16 @@ pub struct PageTable(pub [PageTableEntry; 512]);
 #[repr(C, packed)]
 #[derive(Clone, Copy, Debug, Eq, PartialEq)]
 pub struct PageTableEntry(u64);
+
+/// Get the physical address of the first page table used by a processor.
+/// Multiple processors may use the same page table, but this is not
+/// necessarily the case.
+pub fn first_page_table(processor: &Processor) -> PhysicalAddress {
+    // The physical address of the first page table is stored in bits
+    // [12 .. 64).
+    let mask = !0u64 >> 12 & !0u64 << (64 - 64);
+    PhysicalAddress((processor.cr3() & mask) as *mut u8)
+}
 
 impl PageTableEntry {
     /// Get the physical address stored in a page table entry. This address may
